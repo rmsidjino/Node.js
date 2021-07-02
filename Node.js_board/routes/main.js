4aecf3d437f56b92114c2420310b7f2561b8bb14
Your Name <you@example.com>
2021-07-02 17:29:39 +0900

    Fifth

--- Node.js_board/routes/main.js
+++ Node.js_board/routes/main.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
+var moment = require('moment');
 
 var connection = mysql.createConnection({
     host     : 'localhost', //127.0.0.1
@@ -18,10 +19,12 @@ router.post('/add_article2',function(req, res){
     var No =req.body.No;
     var title =req.body.title;
     var content =req.body.content;
+    var image =req.body.image;
+    var today = moment().format('YYYY-MM-DD');
     console.log(No, title, content);
     connection.query(
-        `insert into board (No, title, content) values (?, ?, ?)`,
-            [No, title, content]);
+        `insert into board (No, title, content,image, today) values (?, ?, ?, ?, ?)`,
+            [No, title, content, image, today]);
     res.redirect("/");
 })
 
@@ -29,7 +32,7 @@ router.get("/", function(req, res){
     connection.query(
         `select * from board`,
         function(err,result){
-            console.log(result);
+            console.log(result.length);
             if(err){
                 console.log(err);
                 res.send("board connection Error");
@@ -52,6 +55,60 @@ router.get("/detail", function(req, res){
                 res.send("board connection Error");
             }else{
                 res.render("search", {article : result});
+            }
+        }
+    )
+})
+
+router.get('/delete',function(req, res){
+    var No =req.query.No;
+    console.log(No)
+    connection.query(
+        `delete from board where No = ?`,
+        [No],
+        function(err,result){
+            if(err){
+                console.log(err);
+                res.send("board connection Error");
+            }else{
+                res.redirect("/");
+            }
+        }
+    )
+})
+
+router.get('/edit',function(req, res){
+    var No =req.query.No;
+    console.log(No)
+    connection.query(
+        `select * from board where No = ?`,
+        [No],
+        function(err,result){
+            if(err){
+                console.log(err);
+                res.send("board connection Error");
+            }else{
+                res.render("edit",{article : result})
+            }
+        }
+    )
+})
+
+router.post('/edit/:No',function(req, res){
+    console.log("edit");
+    var No =req.body.No;
+    var title =req.body.title;
+    var content =req.body.content;
+    var image =req.body.image;
+    connection.query(
+        `update board set title=?, content =?, image=? where No =?`,        
+        [title, content,image, No],
+        function(err,result){
+            if(err){
+                console.log(err);
+                res.send("board connection Error");
+            }else{
+                res.redirect("/");
             }
         }
     )
--- Web_practice/index.js
+++ Web_practice/index.js
@@ -1,5 +1,4 @@
-v
-ar express = require("express");
+var express = require("express");
 var path = require("path");
 var app = express();
 var port = 3000;
